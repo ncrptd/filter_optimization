0e632fcd8d44235c3ab2cbf3ca4ac01e4bf18c21
Rockey Biswas <85108386+ncrptd@users.noreply.github.com>
2023-07-22 12:55:49 +0530

    ref:

--- src/contexts/FilterContext.tsx
+++ src/contexts/FilterContext.tsx
@@ -32,7 +32,7 @@ export const FilterProvider = ({ children }: FilterContextProps) => {
   const [state, dispatch] = useReducer(filterReducer, initialState);
 
   const getData = async () => {
-    const res = await fetch('public/data/dataset_small.csv');
+    const res = await fetch('/data/dataset_small.csv');
     const rawData = await res.text();
     const parsedData = Papa.parse(rawData).data as Array<Array<string>>;
 
